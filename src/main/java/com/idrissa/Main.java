80d40527ef3a665f6eac4b077ee31d7811fd4367
Muvunyi Idrissa <muvunyiiddy@gmail.com>
2023-04-27 13:30:15 +0200

    initialize first endpoint

--- src/main/java/com/idrissa/Main.java
+++ src/main/java/com/idrissa/Main.java
@@ -3,10 +3,40 @@ package com.idrissa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+@GetMapping("/api/v1/listing")
+    public GreetingFn greeting() {
+        return new GreetingFn("Hello spring boot");
+    }
+
+//    record GreetingFn(String greeting){}
+
+    class GreetingFn{
+        private final String greeting;
+
+        GreetingFn(String greeting) {
+            this.greeting = greeting;
+        }
+
+        public String getGreeting() {
+            return greeting;
+        }
+
+        @Override
+        public String toString() {
+            return "GreetingFn{" +
+                    "greeting='" + greeting + '\'' +
+                    '}';
+        }
+
+
     }
 }
